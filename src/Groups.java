58145e71a3beabd5b7237031d6f203213bd050e3
jlee512 <jlee512@aucklanduni.ac.nz>
2017-04-29 23:32:54 +1200

    groupCheck method finalised

--- src/Groups.java
+++ src/Groups.java
@@ -7,7 +7,11 @@ public class Groups {
 
     public static boolean groupCheck(String s) {
 
-        
+        Map <Character, Character> bracePairing = new HashMap<>();
+
+        bracePairing.put(')', '(');
+        bracePairing.put(']', '[');
+        bracePairing.put('}', '{');
 
         Deque <Character> openingBraces = new ArrayDeque<>();
 
@@ -15,11 +19,18 @@ public class Groups {
             if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
                 openingBraces.addFirst(s.charAt(i));
             } else if (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}'){
-                openingBraces.poll(s.charAt(i));
+                if (openingBraces.peek() == bracePairing.get(s.charAt(i))){
+                    openingBraces.remove();
+                } else {
+                    return false;
+                }
             }
         }
-
-        return true;
+        if (openingBraces.peek() == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
@@ -27,12 +38,15 @@ public class Groups {
         String s1 = "{}";
         String s2 = "{]";
         String s3 = "ABC()([]{})";
+        String s4 = "()[]{({[]})}";
         boolean test1 = Groups.groupCheck(s1);
         boolean test2 = Groups.groupCheck(s2);
         boolean test3 = Groups.groupCheck(s3);
+        boolean test4 = Groups.groupCheck(s4);
         System.out.println(test1);
         System.out.println(test2);
         System.out.println(test3);
+        System.out.println(test4);
     }
 
 }
